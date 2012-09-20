e1f3fa37028feb5a9be3a114a22dd7adb9f07e61
Stéphane Nicolas <steff.nicolas@gmail.com>
2012-09-20 17:06:42 +0200

    removed target dirs

--- robospice-sample-it/target/generated-sources/r/com/octo/android/robospice/test/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.test;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
--- robospice-sample/target/generated-sources/r/com/octo/android/robospice/sample/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.octo.android.robospice.sample;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}
