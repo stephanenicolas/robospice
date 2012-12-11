f419bcdddd89e55397499bab7b361aab76ffee41
Fernando Franco Giráldez <ffrancogiraldez@aurigae.com>
2012-12-11 08:18:12 +0100

    Make CachedSpiceRequest handle any kind of cacheKey using Object type

--- robospice/src/com/octo/android/robospice/request/CachedSpiceRequest.java
+++ robospice/src/com/octo/android/robospice/request/CachedSpiceRequest.java
@@ -10,12 +10,12 @@ import com.octo.android.robospice.request.listener.RequestStatus;
 
 public class CachedSpiceRequest< RESULT > extends SpiceRequest< RESULT > {
 
-    private String requestCacheKey = UUID.randomUUID().toString();
+    private Object requestCacheKey = UUID.randomUUID().toString();
     private long cacheDuration;
     private SpiceRequest< RESULT > spiceRequest;
     private boolean isProcessable = true;
 
-    public CachedSpiceRequest( SpiceRequest< RESULT > contentRequest, String requestCacheKey, long cacheDuration ) {
+    public CachedSpiceRequest( SpiceRequest< RESULT > contentRequest, Object requestCacheKey, long cacheDuration ) {
         super( contentRequest.getResultType() );
         this.requestCacheKey = requestCacheKey;
         this.cacheDuration = cacheDuration;
@@ -86,7 +86,7 @@ public class CachedSpiceRequest< RESULT > extends SpiceRequest< RESULT > {
         spiceRequest.publishProgress( progress );
     }
 
-    public String getRequestCacheKey() {
+    public Object getRequestCacheKey() {
         return requestCacheKey;
     }
 
