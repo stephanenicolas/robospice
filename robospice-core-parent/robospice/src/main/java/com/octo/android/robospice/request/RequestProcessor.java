96555abac8c1554a10b13cb3fd741baff4b26c16
Andrew Clark <andrew.clark@bjss.com>
2013-06-30 19:38:34 +0100

    Added RequestNotFoundListener to addListenerIfPending method you can now be notififed when the request doesn't exist

--- robospice-core-parent/robospice/src/main/java/com/octo/android/robospice/request/RequestProcessor.java
+++ robospice-core-parent/robospice/src/main/java/com/octo/android/robospice/request/RequestProcessor.java
@@ -30,6 +30,7 @@ import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.priority.PriorityRunnable;
 import com.octo.android.robospice.request.listener.RequestCancellationListener;
 import com.octo.android.robospice.request.listener.RequestListener;
+import com.octo.android.robospice.request.listener.RequestNotFoundListener;
 import com.octo.android.robospice.request.listener.RequestProgress;
 import com.octo.android.robospice.request.listener.RequestProgressListener;
 import com.octo.android.robospice.request.listener.RequestStatus;
@@ -137,8 +138,11 @@ public class RequestProcessor {
             if (listRequestListenerForThisRequest != null) {
                 listRequestListenerForThisRequest.addAll(listRequestListener);
             }
+
             if (request.isProcessable()) {
                 notifyListenersOfRequestProgress(request, listRequestListener, request.getProgress());
+            } else if (listRequestListenerForThisRequest == null) {
+                notifyListenersOfRequestNofFound(request, listRequestListener);
             }
         }
 
@@ -348,6 +352,21 @@ public class RequestProcessor {
 
     private void post(final Runnable r, final Object token) {
         handlerResponse.postAtTime(r, token, SystemClock.uptimeMillis());
+    }
+
+    private void notifyListenersOfRequestNofFound(final CachedSpiceRequest<?> request, final Set<RequestListener<?>> listRequestListener) {
+        Ln.d("Request was not found when adding request listeners to existing requests. Now try and call onRequestNotFound");
+    
+        for (final RequestListener<?> listener: listRequestListener) {
+            if (listener instanceof RequestNotFoundListener) {
+                post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((RequestNotFoundListener) listener).onRequestNotFound();
+                    }
+                }, request.getRequestCacheKey());
+            }
+        }
     }
 
     private <T> void notifyListenersOfRequestProgress(final CachedSpiceRequest<?> request, final Set<RequestListener<?>> listeners, final RequestStatus status) {
---
+++ robospice-core-parent/robospice/src/main/java/com/octo/android/robospice/request/listener/RequestNotFoundListener.java
@@ -0,0 +1,5 @@
+package com.octo.android.robospice.request.listener;
+
+public interface RequestNotFoundListener {
+    void onRequestNotFound();
+}
