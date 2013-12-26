acb502b1fbc8bdf2c31af3b30a4aa96b35faa2c2
vlad.shvaydetskiy <vlad.shvaydetskiy@filmon.com>
2013-12-26 13:54:52 +0200

    Jackson parser for retrofit extension

---
+++ extensions/robospice-retrofit-parent/robospice-retrofit/src/main/java/com/octo/android/robospice/retrofit/RetrofitJacksonSpiceService.java
@@ -0,0 +1,51 @@
+package com.octo.android.robospice.retrofit;
+
+import android.app.Application;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.octo.android.robospice.persistence.CacheManager;
+import com.octo.android.robospice.persistence.exception.CacheCreationException;
+import com.octo.android.robospice.persistence.retrofit.RetrofitObjectPersisterFactory;
+
+import java.io.File;
+
+import retrofit.RestAdapter;
+import retrofit.RestAdapter.Builder;
+import retrofit.converter.Converter;
+import retrofit.converter.JacksonConverter;
+
+/**
+ * A pre-set, easy to use, retrofit service. It will use retrofit for network
+ * requests and both networking and caching will use Jackson. To use it, just add
+ * to your manifest :
+ * 
+ * <pre>
+ * &lt;service
+ *   android:name="com.octo.android.robospice.retrofit.RetrofitJacksonSpiceService"
+ *   android:exported="false" /&gt;
+ * </pre>
+ * @author Vlad Shvaydetskiy
+ */
+public abstract class RetrofitJacksonSpiceService extends RetrofitSpiceService {
+
+    private Converter converter = new JacksonConverter(new ObjectMapper());
+
+    @Override
+    public CacheManager createCacheManager(Application application) throws CacheCreationException {
+        CacheManager cacheManager = new CacheManager();
+        cacheManager.addPersister(new RetrofitObjectPersisterFactory(application, converter, getCacheFolder()));
+        return cacheManager;
+    }
+
+    @Override
+    protected Builder createRestAdapterBuilder() {
+        RestAdapter.Builder restAdapter = super.createRestAdapterBuilder();
+        restAdapter.setConverter(converter);
+        return restAdapter;
+    }
+
+    public File getCacheFolder() {
+        return null;
+    }
+
+}
