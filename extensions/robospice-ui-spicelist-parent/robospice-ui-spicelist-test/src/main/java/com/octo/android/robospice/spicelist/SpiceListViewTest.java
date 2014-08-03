8f8535351af72758bfa40f6c2a862577b7765d4f
Stéphane Nicolas <steff.nicolas@gmail.com>
2014-08-03 14:51:42 -0400

    Fix checkstyle violations. Ready to release 1.4.13

--- extensions/robospice-ui-spicelist-parent/robospice-ui-spicelist-test/src/main/java/com/octo/android/robospice/spicelist/SpiceListViewTest.java
+++ extensions/robospice-ui-spicelist-parent/robospice-ui-spicelist-test/src/main/java/com/octo/android/robospice/spicelist/SpiceListViewTest.java
@@ -8,43 +8,44 @@ import android.widget.AbsListView.OnScrollListener;
 
 public class SpiceListViewTest extends InstrumentationTestCase {
 
-	private SpiceListView spiceListView;
-	private OnScrollListener mockOnScrollListener;
+    private static final int SCROLL_Y_AMOUNT_IN_PIXELS = 100;
+    private SpiceListView spiceListView;
+    private OnScrollListener mockOnScrollListener;
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		spiceListView = new SpiceListView(getInstrumentation().getContext());
-		mockOnScrollListener = EasyMock.createMock(OnScrollListener.class);
-	}
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        spiceListView = new SpiceListView(getInstrumentation().getContext());
+        mockOnScrollListener = EasyMock.createMock(OnScrollListener.class);
+    }
 
-	@UiThreadTest
-	public void testOnScroll_triggers_wrapped_listener() {
-		//GIVEN
-		mockOnScrollListener.onScroll(spiceListView, 0,0,0);
-		EasyMock.replay(mockOnScrollListener);
+    @UiThreadTest
+    public void testOnScroll_triggers_wrapped_listener() {
+        //GIVEN
+        mockOnScrollListener.onScroll(spiceListView, 0, 0, 0);
+        EasyMock.replay(mockOnScrollListener);
 
-		//WHEN
-		spiceListView.setOnScrollListener(mockOnScrollListener);
-		spiceListView.scrollTo(0, 100);
+        //WHEN
+        spiceListView.setOnScrollListener(mockOnScrollListener);
+        spiceListView.scrollTo(0, SCROLL_Y_AMOUNT_IN_PIXELS);
 
-		//THEN
-		EasyMock.verify(mockOnScrollListener);
-	}
+        //THEN
+        EasyMock.verify(mockOnScrollListener);
+    }
 
-	@UiThreadTest
-	public void testOnFling_triggers_wrapped_listener() throws InterruptedException {
-		//GIVEN
-		mockOnScrollListener.onScroll(spiceListView, 0,0,0);
-		mockOnScrollListener.onScrollStateChanged(spiceListView, OnScrollListener.SCROLL_STATE_FLING);
-		EasyMock.replay(mockOnScrollListener);
-		
-		//WHEN
-		spiceListView.setOnScrollListener(mockOnScrollListener);
-		spiceListView.onScrollListener.onScrollStateChanged(spiceListView, OnScrollListener.SCROLL_STATE_FLING);
+    @UiThreadTest
+    public void testOnFling_triggers_wrapped_listener() throws InterruptedException {
+        //GIVEN
+        mockOnScrollListener.onScroll(spiceListView, 0, 0, 0);
+        mockOnScrollListener.onScrollStateChanged(spiceListView, OnScrollListener.SCROLL_STATE_FLING);
+        EasyMock.replay(mockOnScrollListener);
+        
+        //WHEN
+        spiceListView.setOnScrollListener(mockOnScrollListener);
+        spiceListView.onScrollListener.onScrollStateChanged(spiceListView, OnScrollListener.SCROLL_STATE_FLING);
 
-		//THEN
-		EasyMock.verify(mockOnScrollListener);
-	}
+        //THEN
+        EasyMock.verify(mockOnScrollListener);
+    }
 
 }
